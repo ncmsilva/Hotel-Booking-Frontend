31491563ce77429b778e1001a4a7e49aa6436813
ncmsilva <chathurasilva98@gmail.com>
2024-11-18 00:04:20 +0530

    Login error added

--- src/componants/pages/login/login.jsx
+++ src/componants/pages/login/login.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./login.css";
 import axios from "axios";
 import { Link, Route, Routes } from "react-router-dom";
 import SignUp from "../register/signup";
+import Header from "../../header/header";
 
 export default function LoginPage() {
   const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
+  const [IsUpdated, setIsUpdated] = useState(false);
+  const [error, setError] = useState("");
+
+useEffect(() => {
+  if(!IsUpdated)
+  {
+    setIsUpdated(true);
+  }
+}, [error])
 
   function handdleLogin() {
     const loginUrl = import.meta.env.VITE_DOMAIN + import.meta.env.VITE_LOGIN
@@ -24,16 +34,27 @@ export default function LoginPage() {
           window.location.href = "/admin";
         }else if (rs.data.user.type == "customer") {
         window.location.href = "/home";
-        }
+        }else{
+          <>          
+          <div className='h-screen w-full bg-[#DAE2B6] relative flex-col'>
+            <Header/>
+            <div className='absolute inset-0 flex justify-center items-center'>
+              <h1 className='text-[70px] text-[#FFFBE9]'>Page Under Maintenance.</h1>
+            </div>
+          </div>
+          </>
+          }
       })
       .catch((err) => {
-        console.log(err);
+        setError(err.response.data.message)
+        console.log(err.response.data.message);
+        setIsUpdated(false)
       });
   }
 
   return (
     <div className="h-[100vh] w-full loginbgb flex justify-center items-center">
-      <div className="relative w-[400px] h-[400px] rounded-xl backdrop-blur-md flex flex-col items-center justify-center border-[1px] border-[#00000000] hover:border-black">
+      <div className="relative w-[400px] h-[475px] rounded-xl backdrop-blur-md flex flex-col items-center justify-center border-[1px] border-[#00000000] hover:border-black">
         <h1 className="absolute top-[40px] text-[#8da8c3] text-3xl font-bold text-center p-[15px]">
           Login
         </h1>
@@ -55,13 +76,19 @@ export default function LoginPage() {
             setpassword(e.target.value);
           }}
         />
-        <div className="flex w-[78%] justify-between mt-5 ml-2 mr-2">
+        <div className="flex w-[78%] h-[50px] justify-between mt-5 ml-2 mr-2">
           <Link to="/forgot-password" className="text-black">
             Forgot Password
           </Link>
           <Link to="/signup" className="text-black">
             SignUp
           </Link>
+          
+        </div>
+        <div className="flex w-[78%] justify-between mt-1 ml-2 mr-2">
+          <p className="text-red-500 h-[50px] text-center">
+            {error}
+          </p>
         </div>
         <button
           className="w-[80%] w-[80%] absolute bottom-[40px] bg-red-300 hover:bg-red-500 text-white font-bold py-2 px-4 rounded"
